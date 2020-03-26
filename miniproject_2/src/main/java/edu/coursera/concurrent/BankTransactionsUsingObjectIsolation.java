b9cf5ab2a955883fcf0c161bd99f2e573561e254
mdzhezhelo <RhenjqGfhjkm2013>
2020-03-26 16:46:33 +0200

    made isolation;

--- miniproject_2/src/main/java/edu/coursera/concurrent/BankTransactionsUsingObjectIsolation.java
+++ miniproject_2/src/main/java/edu/coursera/concurrent/BankTransactionsUsingObjectIsolation.java
@@ -1,5 +1,7 @@
 package edu.coursera.concurrent;
 
+import static edu.rice.pcdp.PCDP.isolated;
+
 /**
  * A thread-safe transaction implementation using object-based isolation.
  */
@@ -11,12 +13,6 @@ public final class BankTransactionsUsingObjectIsolation
     @Override
     public void issueTransfer(final int amount, final Account src,
                               final Account dst) {
-        /*
-         * TODO implement issueTransfer using object-based isolation instead of
-         * global isolation, based on the reference code provided in
-         * BankTransactionsUsingGlobalIsolation. Keep in mind that isolation
-         * must be applied to both src and dst.
-         */
-        throw new UnsupportedOperationException();
+        isolated(src, dst, () -> src.performTransfer(amount, dst));
     }
 }
